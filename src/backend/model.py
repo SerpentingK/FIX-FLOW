5346d984af9fd258f032d2e1dbdb43acaa28b7e1
David Carrillo <david.carrillo.moncayo@gmail.com>
2024-09-18 12:12:08 -0500

    Squashed commit of the following:
    commit b764a6753f41d0edd9a12c10bd43a5b4b968ee14
    Merge: 7cf4337 7e544f9
    Author: felsim19 <117236193+felsim19@users.noreply.github.com>
    Date:   Wed Sep 18 12:10:36 2024 -0500
    
        Merge pull request #9 from SerpentingK/Felipe_Sierra
    
        Felipe sierra
    
    commit 7e544f94b449a27f5bc518879d1e30f2464dceb1
    Author: felsim19 <Felipesierra49@gmail.com>
    Date:   Wed Sep 18 11:59:34 2024 -0500
    
        backend mejoras
    
    commit 4f87c3320ed08f0522616a2d04030cee76fca273
    Author: felsim19 <Felipesierra49@gmail.com>
    Date:   Tue Sep 17 19:53:43 2024 -0500
    
        Backend Login
    
    commit 41ef1296cfa5b580e887befec6c053b26ee9a223
    Merge: 58b4a2f 7cf4337
    Author: felsim19 <Felipesierra49@gmail.com>
    Date:   Tue Sep 17 15:56:21 2024 -0500
    
        Merge remote-tracking branch 'origin/main' into Felipe_Sierra
    
    commit 7cf4337aaa2bd46531b76a63bfe0a7cb48ed416b
    Merge: 5552c5c edd083f
    Author: David Carrillo <113156868+SerpentingK@users.noreply.github.com>
    Date:   Sat Sep 14 16:07:46 2024 -0500
    
        Merge pull request #8 from SerpentingK/David_Carrillo
    
        David carrillo
    
    commit 58b4a2f93e16c55b4737fb12eb74844a9ae4053f
    Author: felsim19 <Felipesierra49@gmail.com>
    Date:   Thu Sep 12 23:05:41 2024 -0500
    
        Update nav-bar.vue
    
    commit f51ed4cb6cd24bacbf27de98fb82f82da3c5cd01
    Author: felsim19 <Felipesierra49@gmail.com>
    Date:   Thu Sep 12 23:02:08 2024 -0500
    
        fix
    
    commit de1f36739bb8654d9be1f2e45ad9b60d945edf56
    Merge: bfd0d6d 5552c5c
    Author: felsim19 <Felipesierra49@gmail.com>
    Date:   Thu Sep 12 22:57:24 2024 -0500
    
        Merge remote-tracking branch 'origin/main' into Felipe_Sierra
    
    commit 5552c5ca42b4b69d06ca2ba7b5f90d12efacd730
    Merge: 6012144 f764b05
    Author: David Carrillo <113156868+SerpentingK@users.noreply.github.com>
    Date:   Thu Sep 12 22:44:53 2024 -0500
    
        Merge pull request #7 from SerpentingK/David_Carrillo
    
        David carrillo
    
    commit bfd0d6dd671229b6a402032ce9a61b67aa22199f
    Author: felsim19 <Felipesierra49@gmail.com>
    Date:   Thu Sep 12 22:38:09 2024 -0500
    
        Update list-spare.vue
    
    commit d7dfe2f26c4f3da1ce4c5e2bcb85dcb8e31bbb9e
    Author: felsim19 <Felipesierra49@gmail.com>
    Date:   Thu Sep 12 22:32:09 2024 -0500
    
        Update list-spare.vue
    
    commit 8592ab22ce285411c5dfc8093d7e57b4524dd15b
    Author: felsim19 <Felipesierra49@gmail.com>
    Date:   Thu Sep 12 22:09:53 2024 -0500
    
        Update list-spare.vue
    
    commit 701a686852c2596fd7136e8cd06ed58c85a30874
    Author: felsim19 <Felipesierra49@gmail.com>
    Date:   Thu Sep 12 21:07:29 2024 -0500
    
        component-parts
    
    commit 75e5e9b05d20848191f464f5fcf8a25fc5837820
    Author: felsim19 <Felipesierra49@gmail.com>
    Date:   Thu Sep 12 16:59:15 2024 -0500
    
        Parte 0.2 repuestos
    
    commit 2a94ef5d53f2f65d1c6356c1b6cd9f923ea8fae0
    Author: felsim19 <Felipesierra49@gmail.com>
    Date:   Thu Sep 12 10:41:42 2024 -0500
    
        Primera parte Repuestos
    
    commit 6012144f34f4aa64f6a3f5006a87dec7a49869b7
    Merge: 041181b cea9519
    Author: David Carrillo <113156868+SerpentingK@users.noreply.github.com>
    Date:   Wed Sep 11 08:42:56 2024 -0500
    
        Merge pull request #6 from SerpentingK/Mateo_Gamboa
    
        Eliminacion routers tecnicos
    
    commit cea9519860ce882c5cdb036efdd667921233a394
    Author: David Carrillo <david.carrillo.moncayo@gmail.com>
    Date:   Wed Sep 11 08:41:40 2024 -0500
    
        Eliminacion routers tecnicos
    
    commit 041181b5b0f3c1cc01eef631e040eb6a3c6dae70
    Merge: 483ea5a 707675e
    Author: David Carrillo <113156868+SerpentingK@users.noreply.github.com>
    Date:   Wed Sep 11 08:39:27 2024 -0500
    
        Merge pull request #5 from SerpentingK/David_Carrillo
    
        David carrillo

--- src/backend/model.py
+++ src/backend/model.py
@@ -1,10 +1,8 @@
-from sqlalchemy import String, ForeignKey, Column
+from sqlalchemy import String, Column
 from connection import base
-from sqlalchemy.orm import relationship
 
-
-class TableCompany(base):
-    __tablename__= "company"
-    company_user = Column(String(30), primary_key=True, nullable=False)
-    mail = Column(String(200), nullable=False)
-    pasw = Column(String(50), nullable=False)
+class companyRegistration(base):
+    __tablename__ = "company"
+    company_user = Column(String(50), primary_key=True)
+    mail = Column(String(200), nullable=False, unique=True)
+    password = Column(String(80), nullable=False)
