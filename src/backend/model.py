2db126e3f67f749cc60562f41f68984fd5720306
David Carrillo <david.carrillo.moncayo@gmail.com>
2024-09-21 10:40:05 -0500

    a

--- src/backend/model.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import String, Column
-from connection import base
-
-class companyRegistration(base):
-    __tablename__ = "company"
-    company_user = Column(String(50), primary_key=True)
-    mail = Column(String(200), nullable=False, unique=True)
-    password = Column(String(80), nullable=False)
