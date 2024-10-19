2b18b21c5eb2bc9e4522d17327ac5f490a84f859
David Carrillo <david.carrillo.moncayo@gmail.com>
2024-10-19 13:59:40 -0500

    Squashed commit of the following:
    commit 2da6d66d136c52d2395e1aff9b05cbb28e30c6ba
    Author: David Carrillo <david.carrillo.moncayo@gmail.com>
    Date:   Sat Oct 19 13:55:34 2024 -0500
    
        a
    
    commit 7f8fe5d81257b98d26abf0f17dea0efb19ca5bba
    Author: David Carrillo <david.carrillo.moncayo@gmail.com>
    Date:   Tue Oct 15 22:08:21 2024 -0500
    
        a
    
    commit c6dba206d7c7f05386bc08f8590af5317fd10038
    Author: David Carrillo <david.carrillo.moncayo@gmail.com>
    Date:   Mon Oct 14 22:31:10 2024 -0500
    
        Mejoras en informacion de factura

--- src/components/checks/bill-list.vue
+++ src/components/checks/bill-list.vue
@@ -1,101 +1,120 @@
 <script setup>
-import { ref, provide } from "vue";
-import bill_li from "./bill-li.vue";
-import bill_info from "./bill-info.vue";
+import { ref, provide } from 'vue';
+import bill_li from './bill-li.vue';
+import bill_info from './bill-info.vue';
+import pay_window from './pay-window.vue';
 
 const options = ref(["N. Factura", "Cliente", "Fecha"]);
 const selectedOption = ref(options.value[0]); // Mantiene el valor seleccionado
 
 const show_letter_switch = ref(false);
 const show_bill_info = () => {
-  show_letter_switch.value = !show_letter_switch.value;
-};
+    show_letter_switch.value = !show_letter_switch.value;       
+}
+
+const show_pay_window = ref(false)
+const switch_spw = () => {
+    show_pay_window.value = !show_pay_window.value
+}
+provide('switch_spw', switch_spw)
+
+provide('show_bill_info', show_bill_info);
 
 provide("show_bill_info", show_bill_info);
 
 const phones_list = [
-  {
-    phone_ref: "0001-A-1",
-    brand: "Apple",
-    device: "IPhone XR",
-    price: 20000,
-    details: "pantalla",
-    delivered: false,
-    repaired: false,
-    delivery_date: null,
-  },
-  {
-    phone_ref: "0001-A-2",
-    brand: "Apple",
-    device: "IPhone 11 PRO",
-    price: 30000,
-    details: "pantalla",
-    delivered: true,
-    repaired: true,
-    delivery_date: "08/06/2024",
-  },
-  {
-    phone_ref: "0001-A-1",
-    brand: "Apple",
-    device: "IPhone XR",
-    price: 20000,
-    details: "pantalla",
-    delivered: false,
-    repaired: false,
-    delivery_date: null,
-  },
-  {
-    phone_ref: "0001-A-1",
-    brand: "Apple",
-    device: "IPhone XR",
-    price: 20000,
-    details: "pantalla",
-    delivered: false,
-    repaired: false,
-    delivery_date: null,
-  },
-  {
-    phone_ref: "0001-A-1",
-    brand: "Apple",
-    device: "IPhone XR",
-    price: 20000,
-    details: "pantalla",
-    delivered: false,
-    repaired: false,
-    delivery_date: null,
-  },
-];
+    {
+        phone_ref: "0001-A-1",
+        brand: "Apple",
+        device: "IPhone XR",
+        price: 20000,
+        details: "pantalla",
+        delivered: false,
+        repaired: false,
+        delivery_date: null
+    },
+    {
+        phone_ref: "0001-A-2",
+        brand: "Apple",
+        device: "IPhone 11 PRO",
+        price: 30000,
+        details: "pantalla",
+        delivered: true,
+        repaired: true,
+        delivery_date: "08/06/2024"
+    },
+    {
+        phone_ref: "0001-A-3",
+        brand: "Apple",
+        device: "IPhone XR",
+        price: 20000,
+        details: "pantalla",
+        delivered: false,
+        repaired: false,
+        delivery_date: null
+    },
+    {
+        phone_ref: "0001-A-4",
+        brand: "Apple",
+        device: "IPhone XR",
+        price: 20000,
+        details: "pantalla",
+        delivered: false,
+        repaired: false,
+        delivery_date: null
+    },
+    {
+        phone_ref: "0001-A-5",
+        brand: "Apple",
+        device: "IPhone XR",
+        price: 20000,
+        details: "pantalla",
+        delivered: false,
+        repaired: true,
+        delivery_date: null
+    }
+]
 </script>
 
 <template>
-  <section class="container">
-    <nav class="search-bar">
-      <form action="" class="search-form">
-        <select id="searchOption" v-model="selectedOption">
-          <option v-for="option in options" :key="option">
-            {{ option }}
-          </option>
-        </select>
-        <input
-          v-if="selectedOption !== 'Fecha'"
-          type="text"
-          placeholder="Buscar..."
-        />
-        <input v-else type="date" />
-        <button type="submit">Buscar</button>
-      </form>
-    </nav>
-    <section class="checks-section">
-      <ul>
-        <bill_li
-          v-for="i in 4"
-          :key="i"
-          :check_num="`A-000${i}`"
-          client_name="Felipe Sierra"
-          check_date="08/06/2004"
-          total_price="50000"
-        ></bill_li>
-      </ul>
+    <section class="container">
+        <nav class="search-bar">
+            <form action="" class="search-form">
+                <select id="searchOption" v-model="selectedOption">
+                    <option v-for="option in options" :key="option">
+                        {{ option }}
+                    </option>
+                </select>
+                <input v-if="selectedOption !== 'Fecha'" type="text" placeholder="Buscar...">
+                <input v-else type="date">
+                <button type="submit">Buscar</button>
+            </form>
+        </nav>
+        <section class="checks-section">
+            <ul>
+                <bill_li v-for="i in 4" :key="i" :check_num="`A-000${i}`" client_name="Felipe Sierra" check_date="08/06/2004" total_price="50000"></bill_li>
+            </ul>
+        </section>
+        <transition-group
+            name="slide-fade"
+            mode="out-in"
+        >
+            <bill_info
+                v-if="show_letter_switch"
+                bill_number="0001-A"
+                client_name="David Carrillo"
+                entry_date="08/06/2024"
+                :total_price="50000"
+                :due="40000"
+                :payment="10000"
+                client_phone="3133680686"
+                wname="srk"
+                :phones_list="phones_list"
+                key="bill-info"
+            />
+
+            <pay_window v-if="show_pay_window"></pay_window>
+        </transition-group>
     </section>
     <transition name="slide-fade" mode="out-in">
       <bill_info
---
+++ src/components/checks/pay-window.vue
@@ -0,0 +1,107 @@
+<script setup>
+import { inject } from 'vue';
+
+defineProps(
+    {
+        bill_num:{
+            type: String,
+            default: "0001-A",
+            required: true
+        }
+    }
+)
+const switch_spw = inject('switch_spw')
+</script>
+
+<template>
+    <section class="cont">
+        <h2>
+            <span>Abono a factura: </span>
+            <span>{{ bill_num }}</span>
+        </h2>
+        <label for="pay-input">
+            <ion-icon name="cash-outline"></ion-icon>
+            <input type="number" id="pay-input" placeholder="Abono" min="0">
+        </label>
+        <div class="buttons-container">
+            <button class="cancel-btn" @click="switch_spw()">Cancelar</button>
+            <button class="okay-btn" @click="switch_spw()">Aceptar</button>
+        </div>
+    </section>
+</template>
+<style scoped>
+.cont{
+    position: fixed;
+    background-color: var(--baseGray);
+    width: 500px;
+    border: 4px solid var(--baseOrange);
+    padding: 30px;
+    border-radius: 30px;
+    box-shadow: var(--baseShadow);
+    margin-top: 50px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 30px;
+}
+label{
+    display: flex;
+    justify-content: center;
+    background-color: white;
+    padding: 20px 30px;
+    gap: 20px;
+    align-items: center;
+    border-radius: 20px;
+    box-shadow: var(--secShadow);
+
+}
+label input{
+    all: unset;
+}
+label input::-webkit-inner-spin-button, label input::-webkit-outer-spin-button{
+    display: none;
+}
+label ion-icon{
+    scale: 1.4;
+}
+h2{
+    display: flex;
+    gap: 20px;
+    text-shadow: var(--baseShadow);
+    font-family: var(--titleFont);
+    background: linear-gradient(90deg,
+            var(--baseOrange), white);
+    background-clip: text;
+    letter-spacing: 1.5px;
+    -webkit-text-fill-color: transparent;
+    font-weight: bolder;
+    text-transform: uppercase;
+}
+.buttons-container{
+    display: flex;
+    justify-content: space-evenly;
+    width: 100%;
+}
+.cancel-btn, .okay-btn{
+    all: unset;
+    cursor: pointer;
+    transition: .3s;
+    border: 2px solid var(--baseOrange);
+    padding: 10px 20px;
+    border-radius: 20px;
+    box-shadow: var(--secShadow);
+    color: white;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    font-weight: bolder;
+}
+.okay-btn:hover{
+    scale: 1.1;
+}
+.cancel-btn:hover{
+    scale: 0.9;
+}
+.okay-btn{
+    background-color: var(--baseOrange);
+}
+</style>
